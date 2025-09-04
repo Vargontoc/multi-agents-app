36eeba78bfa5b79329943a6993039a58072570fc
Vargontoc <atperamato@gmail.com>
2025-09-05 01:21:37 +0200

    chore: Initial project

---
+++ src/main/java/es/agonzalez/multiagent/app/MultiAgentApplication.java
@@ -0,0 +1,14 @@
+package  es.agonzalez.multiagent.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MultiAgentApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MultiAgentApplication.class);
+    }
+}
